689e9b54c5162a3154c92b0f3d9521f6c1ed060d
bruno lino <brunolamegolino@gmail.com>
2023-06-22 22:15:21 -0300

    backend partial commit

--- backend/src/hello.php
+++ backend/src/hello.php
@@ -1,14 +1,11 @@
 <?php
-require_once 'databaseService.php';
 
-$dataService = new DatabaseService();
 
 header('Content-Type: application/json');
-$data = array('key' => 'value', 'key2' => 'value2');
-echo json_encode($data);
 
-// echo '<pre>';
-// var_dump($_SERVER);
-// // exit();
-// echo phpinfo();
-// echo 'Goodbye, world!'.getenv('DB_NAME');
+$data = array('key' => 'value', 'key2' => 'value2');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $data = array('key' => 'value');
+}
+    
+echo json_encode($data);
